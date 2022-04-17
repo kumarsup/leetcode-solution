1f41269fe15260ee9667f02fa2f83abc9f7b3550
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-04-18 06:52:53 +0800

    Time: 33 ms (93.10%), Space: 15.4 MB (34.22%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -14,27 +14,23 @@ head, curr, tail
 class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root: return root
-        head, curr = None, None
-        last = TreeNode(-1)
+        first, last = None, None
         
         def dfs(node):
-            nonlocal head, last, curr
+            nonlocal first, last
             if not node: return 
             dfs(node.left)
-            curr = TreeNode(node.val)
-            
-            if not head:
-                head = TreeNode(-1)
-                head.right = curr
-                
-            curr.left = last
-            last.right = curr
-            last = curr
+            if last:
+                last.right = node
+                node.left = last
+            else:
+                first = node
+            last = node
             dfs(node.right)
             
         dfs(root)
             
-        head.right.left = last
-        last.right = head.right
-        return head.right
+        first.left = last
+        last.right = first
+        return first
             
