5a9e99ef79d2c04cf666b9160fba17bb44321c3b
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-04-18 23:52:58 +0800

    Time: 38 ms (99.75%), Space: 18 MB (90.88%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -13,36 +13,37 @@ stack -
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        res = -1
-        def dfs(node):
-            nonlocal res, k
-            if not node: return
-            dfs(node.left)
+        # res = -1
+        # def dfs(node):
+        #     nonlocal res, k
+        #     if not node: return
+        #     dfs(node.left)
+        #     k -= 1
+        #     if k == 0 and res == -1:
+        #         res = node.val
+        #         return
+        #     dfs(node.right)
+        # dfs(root)
+        # return res
+        
+        
+        def insertLeft(node):
+            nonlocal stack
+            while node:
+                stack.append(node)
+                node = node.left
+        
+        stack = []
+        insertLeft(root)
+        
+        while stack:           
+            node = stack.pop()
             k -= 1
-            if k == 0 and res == -1:
-                res = node.val
-                return
-            dfs(node.right)
-        dfs(root)
-        return res
-                
-        
-        
-        
-#         stack = [root]
-#         while stack:
-#             node = stack.pop()
-#             while node.left:
-#                 node = node.left
-#                 stack.append(node)
-#             k -= 1
-#             if k == 0:
-#                 return node.val
-            
-#             if node.right:
-#                 stack.append(node.right)
-                
-#         return -1
+            if k == 0: 
+                return node.val
+            if node.right: 
+                insertLeft(node.right)
+        return -1
             
             
                 
