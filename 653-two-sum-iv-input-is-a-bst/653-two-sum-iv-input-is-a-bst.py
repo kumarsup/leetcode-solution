d951d38abdd1938bb31f7c240d96735899d0d570
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-04-21 01:21:08 +0800

    Time: 133 ms (37.07%), Space: 16.3 MB (96.24%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -1,15 +1,24 @@
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
         hashset = set()
+        stack = []
         
-        def dfs(node):
-            nonlocal hashset
-            if not node: return False
-            if k - node.val in hashset:  return True
-            hashset.add(node.val)
-            return dfs(node.left) or dfs(node.right)
-        return dfs(root)
-        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
             
-                
-                
+            root = stack.pop()
+            if k - root.val in hashset: return True
+            hashset.add(root.val)
+            root = root.right
+        return False
+            
+#         def dfs(node):
+#             nonlocal hashset
+#             if not node: return False
+#             if k - node.val in hashset:  return True
+#             hashset.add(node.val)
+#             return dfs(node.left) or dfs(node.right)
+        
+#         return dfs(root)
