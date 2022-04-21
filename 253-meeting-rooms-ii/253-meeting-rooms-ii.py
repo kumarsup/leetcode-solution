f12f311e8a61e7a2bee6c37b27d6d35b410afee2
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-04-21 11:59:49 +0800

    Time: 111 ms (45.59%), Space: 17.5 MB (84.71%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,23 +1,32 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key = lambda x: (x[0], x[1]))
+        starts = [val[0] for val in intervals]
+        ends = [val[1] for val in intervals]
+        starts.sort()
+        ends.sort()
         
-        maxSize, counter, heap, n = 0, 0, [], len(intervals)
+        s, e, count = 0, 0, 0
         
-        for i in range(n):
-            start, end = intervals[i]
-            while heap and heap[0] <= intervals[i][0]: heapq.heappop(heap)
-            if not heap or heap[0] > intervals[i][0]: heapq.heappush(heap, intervals[i][1])
-            maxSize = max(maxSize, len(heap))
-        return maxSize
+        while s < len(intervals):
+            if starts[s] < ends[e]:
+                s+=1
+                count+=1
+            else:
+                s+=1
+                e+=1
+        return count
             
-'''
-[[6,17],[8,9],[11,12],[6,9]]
-
-[6,9], [6,17], [8,9], [11,12]
-i = 2
-heap - (9, 6), (9, 8)
-'''      
+        
+        
+        
+        # intervals.sort(key = lambda x: (x[0], x[1]))
+        # maxSize, counter, heap, n = 0, 0, [], len(intervals)
+        # for i in range(n):
+        #     start, end = intervals[i]
+        #     while heap and heap[0] <= intervals[i][0]: heapq.heappop(heap)
+        #     if not heap or heap[0] > intervals[i][0]: heapq.heappush(heap, intervals[i][1])
+        #     maxSize = max(maxSize, len(heap))
+        # return maxSize  
             
-            
+        
         
