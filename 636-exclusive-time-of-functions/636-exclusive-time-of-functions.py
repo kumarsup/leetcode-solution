435791ada3d27d7dc222cb531d16ea14910f7668
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-04-25 22:34:00 +0800

    Time: 103 ms (47.80%), Space: 14.1 MB (35.35%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.py
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.py
@@ -1,22 +1,39 @@
 '''
 "{function_id}:{"start" | "end"}:{timestamp}"
 
+res = [0, 0]
+["0:start:0","1:start:2","1:end:5","0:end:6"]
+
+start - 
+ id = 0
+ time = 6
+ stack = [0]
+ last = 6
+ val = time - last + 1 = 1
+ index = 0
+ res = [3, 4]
+ 
+ 
+ 
+
+ 
 '''
 
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        last, stack, res = 0, [], [0]*n
+        prev, stack, res = 0, [], [0]*n
         
-        for i in range(len(logs)):
-            fid, state, time = logs[i].split(':')
+        for log in logs:
+            fid, state, time = log.split(':')
             fid, time = int(fid), int(time)
-            
             if state == 'start':
-                if stack: 
-                    res[stack[-1]] += time - last
+                if stack:
+                    res[stack[-1]] += time - prev
                 stack.append(fid)
-                last = time
+                prev = time
             else:
-                res[stack.pop()] += time - last + 1
-                last = time + 1
+                res[stack.pop()] += time - prev + 1
+                prev = time + 1
         return res
+                    
+        
