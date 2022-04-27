98afc0991b42cf7c25f35b0437d59d037208ed3a
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-04-28 00:01:39 +0800

    Time: 212 ms (77.64%), Space: 14.7 MB (31.35%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,71 @@
+# class UnionFind:
+#     def __init__(self, size):
+#         self.root = [i for i in range(size)]
+        
+#     def find(self, x):
+#         if x == self.root[x]:
+#             return x
+#         self.root[x] = self.find(self.root[x])
+#         return self.root[x]
+    
+#     def union(self, x, y):
+#         rootX = self.find(x)
+#         rootY = self.find(y)
+        
+#         if rootX != rootY:
+#             self.root[rootY] = rootX
+        
+#     def connected(self, x, y):
+#         return self.find(x) == self.find(y)
+'''
+[1,0,0,1],
+[0,1,1,0],
+[0,1,1,1],
+[1,0,1,1]]
+'''        
+        
+class Solution:
+    def findCircleNum(self, mat: List[List[int]]) -> int:
+        rows, cols = len(mat), len(mat[0])
+        graph = defaultdict(list)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if mat[r][c] == 1:
+                    graph[r].append(c)
+                    graph[c].append(r)
+                
+        def dfs(node, seen):
+            if node not in seen:
+                seen.add(node)
+                for nei in graph[node]:
+                    dfs(nei, seen)
+                    
+        count, seen = 0, set()
+        for r in range(rows):
+            if r not in seen:
+                count += 1
+                dfs(r, seen)
+                
+        for c in range(cols):
+            if c not in seen:
+                count += 1
+                dfs(c, seen)
+                
+        return count
+
+    
+    
+#         n = len(isConnected)
+#         uFind = UnionFind(n)
+#         for r in range(n):
+#             for c in range(n):
+#                 if isConnected[r][c] == 1 and r != c:
+#                     uFind.union(r, c)
+#         count = 0
+#         for r in range(n):
+#             for c in range(n):
+#                 res = uFind.connected(r, c)
+#                 if res:
+#                     count+=1
+#         return count
