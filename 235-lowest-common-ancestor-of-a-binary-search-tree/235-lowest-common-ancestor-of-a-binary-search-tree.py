9144597b4e74bc7bddacc7c0d0a50818673945b9
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-01 15:42:11 +0800

    Time: 108 ms (46.13%), Space: 18.9 MB (23.68%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,10 +6,19 @@ class Solution:
         if not root: return root
         def dfs(node):
             if not node: return None
-            left = dfs(node.left)
-            right = dfs(node.right)
-            if (left and right) or node == p or node == q:
+            
+            left, right = None, None
+            
+            if node.val > p.val or node.val > q.val:
+                left = dfs(node.left)
+                
+            if node.val < p.val or node.val < q.val:    
+                right = dfs(node.right)
+                
+            if (left and right) or node in [p, q]:
                 return node
+            
             return left or right
+        
         return dfs(root)
             
