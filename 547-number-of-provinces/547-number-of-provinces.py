0a5281eeff1860f0b5bf6ffcf2d899a20e3cc167
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-01 20:40:59 +0800

    Time: 365 ms (19.09%), Space: 14.3 MB (78.20%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -4,7 +4,7 @@ class UnionFind:
         self.count = size
         
     def find(self, x):
-        if x == self.root[x]:
+        while x == self.root[x]:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
@@ -14,23 +14,18 @@ class UnionFind:
         rootY = self.find(y)
         if rootX != rootY:
             self.root[rootY] = rootX
-            self.count -= 1
-        
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-'''
-[1,0,0,1],
-[0,1,1,0],
-[0,1,1,1],
-[1,0,1,1]]
-'''        
-        
+            self.count-=1
+
+            
 class Solution:
     def findCircleNum(self, mat: List[List[int]]) -> int:
         rows, cols = len(mat), len(mat[0])
-        uFind = UnionFind(rows)
+        unionFind = UnionFind(rows)
+        
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 1 and r != c:
-                    uFind.union(r, c)
-        return uFind.count
+                    unionFind.union(r, c)
+        return unionFind.count
+                    
+        
