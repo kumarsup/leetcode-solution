1160f2f6b39cd6715c0cefbb01f5289b5d8ba161
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-01 15:58:39 +0800

    Time: 214 ms (75.35%), Space: 14.3 MB (78.20%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,6 +1,7 @@
 class UnionFind:
     def __init__(self, size):
         self.root = [i for i in range(size)]
+        self.count = size
         
     def find(self, x):
         if x == self.root[x]:
@@ -11,9 +12,9 @@ class UnionFind:
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
-        
         if rootX != rootY:
             self.root[rootY] = rootX
+            self.count -= 1
         
     def connected(self, x, y):
         return self.find(x) == self.find(y)
@@ -27,43 +28,9 @@ class UnionFind:
 class Solution:
     def findCircleNum(self, mat: List[List[int]]) -> int:
         rows, cols = len(mat), len(mat[0])
-#         graph = defaultdict(list)
-#         for r in range(rows):
-#             for c in range(cols):
-#                 if mat[r][c] == 1:
-#                     graph[r].append(c)
-#                     graph[c].append(r) 
-#         def dfs(node, seen):
-#             if node not in seen:
-#                 seen.add(node)
-#                 for nei in graph[node]:
-#                     dfs(nei, seen)               
-#         count, seen = 0, set()
-#         for r in range(rows):
-#             if r not in seen:
-#                 count += 1
-#                 dfs(r, seen)              
-#         for c in range(cols):
-#             if c not in seen:
-#                 count += 1
-#                 dfs(c, seen)            
-#         return count
-
-    
         uFind = UnionFind(rows)
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 1 and r != c:
                     uFind.union(r, c)
-        count = 0
-        provinces = set()
-        
-        for r in range(rows):
-            root = uFind.find(r)
-            provinces.add(root)
-            
-        for c in range(cols):
-            root = uFind.find(c)
-            provinces.add(root)
-      
-        return len(provinces)
+        return uFind.count
