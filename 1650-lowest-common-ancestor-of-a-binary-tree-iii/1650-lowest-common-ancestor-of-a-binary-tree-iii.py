20a24baccce690e25bee5ca2c49bf4088e901ec0
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-05 20:52:03 +0800

    Time: 108 ms (31.72%), Space: 18.4 MB (33.62%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -7,28 +7,30 @@ class Node:
         self.right = None
         self.parent = None
 """
-
 '''
-right
-'''
+lowest common ancestor-  meanse? - lowset common node which is the parent of both the noth node or branch
+    - what if node == p or node == q ?
+    - what if p or q not exist in tree?
+    
+Approach - 
+    - use set- 
+    - start from p and add the node to hashmap
+    - start from q and check if the node exist in hashmap
+    
+TC - O(N)
+SC - O(N)
 
+Approach 2:
+    - traverse p parent or q parent and check until p != q
+    - 
+TC - O(n)
+SC - O(1)
+'''
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         origP, origQ = p, q
-        while p!= q:
+        while p != q:
             p = p.parent if p.parent else origQ
             q = q.parent if q.parent else origP
         return p
-        
-#         hset = set()
-#         node = p
-        
-#         while node:
-#             hset.add(node)
-#             node = node.parent
-#         node = q
-#         while node:
-#             if node in hset: return node
-#             node = node.parent
-#         return None
             
