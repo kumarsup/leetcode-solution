0c1a6f93bf401e3ff57a3319663f0f74c44ec1a4
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-05 20:08:13 +0800

    Time: 111 ms (81.68%), Space: 15.8 MB (45.96%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,20 +1,45 @@
+'''
+s = "lee(t(c ) o ) de )"
+"lee(t(c)o)de"
+
+validate stack - 
+    - add index in stack on start brackets (
+    - pop index in stack on start brackets )
+    - if cant pop then keep the index in stack
+    
+arr = list(s)
+stack = 
+
+validate parentheses
+    if c == '('
+        - stack = add i
+    elif c == ')'
+        if stack: stack pop
+        else
+            arr[i] = ''
+while stack:
+    arr[stack.pop()] = ''
+return ''.join(arr)
+
+TC - O(N)
+SC - O(N)
+
+'''
+
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        n = len(s)
-        stack = []
+        stack, arr = [], list(s)
         
-        for i in range(n):
-            char = s[i]
-            if char == '(':
-                stack.append(i)
-            elif char == ')':
-                if stack and s[stack[-1]] == '(':
+        for index in range(len(arr)):
+            if arr[index] == '(':
+                stack.append(index)
+            elif arr[index] == ')':
+                if stack:
                     stack.pop()
                 else:
-                    stack.append(i)
-        slist = list(s)
-        for idx in stack:
-            slist[idx] = ''
-        return ''.join(slist)
-                
-            
+                    arr[index] = ''
+        while stack:
+            arr[stack.pop()] = ''
+        
+        return ''.join(arr)
+        
