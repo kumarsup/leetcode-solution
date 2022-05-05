543a21eb03c2fc8d62dad01e1b05f7bbf36e25ff
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-05 13:45:20 +0800

    Time: 558 ms (13.78%), Space: 15.4 MB (98.75%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,11 +1,23 @@
 '''
 diff = abs(x - num)
-heap - (dif, num)
+TC - O(n log n) + O(k log k)
+SC - O(N)
+
+
+
 
 '''
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        heap = []
-        res = sorted(arr, key = lambda num: abs(x-num))
-        return sorted(res[:k])
-    
+        n = len(arr)
+        left = 0
+        right = n - k
+        
+        while left < right:
+            mid = (left+right)//2
+            
+            if x - arr[mid] > arr[mid+k] - x:
+                left = mid+1
+            else:
+                right = mid
+        return arr[left: left+k]
