4c49d67914535e6e9bc5c7f462fbd6b8dfb7756b
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-05 13:45:26 +0800

    Time: 436 ms (37.55%), Space: 15.6 MB (43.22%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -15,7 +15,6 @@ class Solution:
         
         while left < right:
             mid = (left+right)//2
-            
             if x - arr[mid] > arr[mid+k] - x:
                 left = mid+1
             else:
