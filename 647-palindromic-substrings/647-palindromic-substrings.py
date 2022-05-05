d64a7f3d7ed9703ec31a9d49b5bd9d49b1810dc5
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-05 12:00:07 +0800

    Time: 192 ms (52.64%), Space: 13.8 MB (77.43%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -4,26 +4,17 @@ class Solution:
         
         def countPalindrome(left, right):
             count = 0
-            while left >= 0 and right < n:
-                if s[left] != s[right]:
-                    break
-                count += 1
-                left -= 1
-                right += 1
+            while left >= 0 and right < n and s[left] == s[right]:
+                left, right, count = left-1, right+1, count+1
             return count
         
-        for idx in range(n):    
+        for idx in range(n):
             res += countPalindrome(idx, idx)
             res += countPalindrome(idx, idx+1)
         return res
         
         
-        
-        
-        
-        
-        
-        
+
         
         
         
