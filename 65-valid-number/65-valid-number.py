4ca2842d51d0f0cd5d4492727f29477c67a74741
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-08 17:03:45 +0800

    Time: 36 ms (85.09%), Space: 13.9 MB (79.43%) - LeetHub

--- 65-valid-number/65-valid-number.py
+++ 65-valid-number/65-valid-number.py
@@ -4,27 +4,31 @@
         - digit, dot, e, sign
         - +/- only can be at 0th index or just after e
         - e should be after digit and before few digit
-        - . can be only once and only before e
+        - . can be only once and only before e        
+
+digitSeen, expoSeen, dotSeen
+for sign = can be the first char or first char after e or E
+return digitSeen
 '''
 class Solution:
     def isNumber(self, s: str) -> bool:
-        seenDigit, seenExpo, seenDot = False, False, False
+        digitSeen, expoSeen, dotSeen = False, False, False
+        n = len(s)
         
-        for i, c in enumerate(s):
-            if c.isdigit():
-                seenDigit = True
-            elif c in '+-':
-                if i > 0 and s[i-1] not in 'eE':
-                    return False
-            elif c in 'eE':
-                if seenExpo or not seenDigit: return False
-                seenExpo = True
-                seenDigit = False
-            elif c == '.':
-                if seenDot or seenExpo: return False
-                seenDot = True
-            else:
+        for i in range(n):
+            if s[i].isdigit():
+                digitSeen = True
+            elif s[i] in '+-':
+                if i > 0 and s[i-1] not in 'eE': return False
+            elif s[i] == '.':
+                if dotSeen or expoSeen: return False
+                dotSeen = True
+            elif s[i] in 'eE':
+                if not digitSeen or expoSeen: return False
+                expoSeen = True
+                digitSeen = False
+            else: 
                 return False
-        return seenDigit
-                
-            
+        return digitSeen
+        
+        
