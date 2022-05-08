cec2fbfb471270cd71e00623ccd64b5e5d80bf6c
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-08 21:30:38 +0800

    Time: 96 ms (5.43%), Space: 16.9 MB (5.14%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,52 @@
+'''
+s = "leetcode", wordDict = ["leet","code"]
+
+Trie= leetcode
+
+search word - o(wn*n)
+
+
+
+'''
+class TrieNode:
+    def __init__(self):
+        self.child = {}
+        self.eow = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.child:
+                node.child[ch] = TrieNode()
+            node = node.child[ch]
+        node.eow = True
+    
+    def search(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.child:
+                return False
+            node.child[ch] = TrieNode()
+            node = node.child[ch]
+        return node.eow
+        
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        trie, dp = Trie(), {}
+        for word in wordDict: trie.insert(word) 
+        def dfs(idx, node):
+            nonlocal trie, dp
+            key = (idx, id(node))
+            if key in dp: return dp[key]
+            
+            if idx >= len(s): return node.eow
+            if node.eow and dfs(idx, trie.root):  return True
+            if s[idx] in node.child and dfs(idx+1, node.child[s[idx]]): return True
+            dp[key] = False
+            return False
+        return dfs(0, trie.root)
+    
