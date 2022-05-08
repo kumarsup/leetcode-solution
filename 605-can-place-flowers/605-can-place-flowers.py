59f1416c521aaab017b815e727c6290cbb8f1fe2
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-09 01:24:48 +0800

    Time: 181 ms (74.57%), Space: 14.3 MB (76.98%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        length, count = len(flowerbed), 0
+        for i in range(length):
+            if flowerbed[i] == 0:
+                leftEmpty = (i == 0 or flowerbed[i-1] == 0)
+                rightEmpty = (i == length-1) or (flowerbed[i+1] == 0)
+                
+                if leftEmpty and rightEmpty:
+                    flowerbed[i] = 1
+                    count+=1
+        return count >= n
