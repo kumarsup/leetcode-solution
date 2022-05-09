7cdab25df51b89bfc6aa801e8020c7a5f57bc115
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-09 12:51:05 +0800

    Time: 368 ms (36.53%), Space: 16.6 MB (33.34%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,15 +1,19 @@
 '''
+prefix sum
+sumMap = {}
+
 
 '''
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        sumA = 0
-        n, hashmap = len(nums), {0: 1}
-        count = 0
+        count, n, sumMap, sumA = 0, len(nums), {0: 1}, 0
         
         for num in nums:
             sumA += num
-            if sumA - k in hashmap:  count += hashmap[sumA-k]
-            if sumA in hashmap: hashmap[sumA] += 1
-            else: hashmap[sumA] = 1
+            
+            if sumA-k in sumMap:
+                count += sumMap[sumA-k]
+            if sumA not in sumMap:
+                sumMap[sumA] = 0
+            sumMap[sumA] += 1
         return count
