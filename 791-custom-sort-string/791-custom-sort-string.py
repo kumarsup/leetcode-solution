50300a751d504ef2d9652262d378698b08b50383
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-09 13:06:53 +0800

    Time: 33 ms (78.62%), Space: 13.9 MB (18.38%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.py
+++ 791-custom-sort-string/791-custom-sort-string.py
@@ -1,10 +1,29 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        orderMap = {}
-        
-        for i in range(len(order)):
-            orderMap[order[i]] = i
+        counter = Counter(s)
+        res = []
+        for c in order:
+            res.append(c*counter[c])
+            counter[c] = 0
+        for c in counter:
+            res.append(c*counter[c])
+        return ''.join(res)
             
-        sList = list(s)
-        sList.sort(key = lambda x: orderMap[x] if x in orderMap else 27)
-        return ''.join(sList)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # orderMap = {}
+        # for i in range(len(order)): orderMap[order[i]] = i
+        # sList = list(s)
+        # sList.sort(key = lambda x: orderMap[x] if x in orderMap else 27)
+        # return ''.join(sList)
