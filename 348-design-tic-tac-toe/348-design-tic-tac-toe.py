c1aca6308a56c6084753e65acb475b444595a440
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-12 16:44:40 +0800

    Time: 121 ms (51.27%), Space: 16.8 MB (20.15%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.py
@@ -10,29 +10,22 @@ class TicTacToe:
     def __init__(self, n: int):
         self.rows = [0]*n
         self.cols = [0]*n
-        self.diagonal = 0
-        self.antidiagonal = 0
+        self.posDiag = 0
+        self.negDiag = 0
         
     def move(self, row: int, col: int, player: int) -> int:
-        curPlayer = 1 if player == 1 else -1
-        
+        currPlayer = 1 if player == 1 else -1
         n = len(self.rows)
         
-        self.rows[row] += curPlayer
-        self.cols[col] += curPlayer
+        self.rows[row] += currPlayer
+        self.cols[col] += currPlayer
         
-        if row == col: 
-            self.diagonal += curPlayer
+        if row == col:
+            self.posDiag += currPlayer
+        
+        if row + col == n-1:
+            self.negDiag += currPlayer
             
-        if row+col == n-1: 
-            self.antidiagonal += curPlayer
-            
-        if (abs(self.rows[row]) == n) or (abs(self.cols[col]) == n) or  (abs(self.diagonal) == n) or (abs(self.antidiagonal) == n):
+        if abs(self.rows[row]) == n or abs(self.cols[col]) == n or abs(self.posDiag) == n or abs(self.negDiag) == n:
             return player
-        
         return 0
-        
-
-# Your TicTacToe object will be instantiated and called as such:
-# obj = TicTacToe(n)
-# param_1 = obj.move(row,col,player)
