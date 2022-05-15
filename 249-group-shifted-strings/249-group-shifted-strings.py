590f0d9a0ae79a510e79b3989c2adaed6e7ae60d
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-15 10:54:15 +0800

    Time: 43 ms (71.89%), Space: 13.9 MB (87.28%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -1,14 +1,37 @@
+'''
+string = ch
+shift = s[0]
+hash = chr((ord(ch)-ord(string[0]))%26 + ord('a'))
+
+xyz - abc
+bcd - abc
+
+
+n = len of arr
+k = max len of one string in arr
+
+TC - O(n*k)
+SC - O(n*k)
+
+'''
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         
         def getHash(string):
             shift = ord(string[0])
-            return ''.join(chr((ord(letter)-shift)%26+ord('a')) for letter in string)
+            hashkey = ''
+            
+            for ch in string:
+                val = chr((ord(ch)-shift)%26 + ord('a'))
+                hashkey = hashkey+val
+                
+            return hashkey
         
-        groups = defaultdict(list)
-        
-        for word in strings:
-            hashKey = getHash(word)
-            groups[hashKey].append(word)
-        return list(groups.values())
+        resultMap = defaultdict(list)
+        for string in strings:
+            hashKey = getHash(string)
+            resultMap[hashKey].append(string)
+            
+        return resultMap.values()
+            
         
