5ebb6b0a4a5e8f3ebfff19a3368d0281ab8fbefa
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-15 19:37:01 +0800

    Time: 328 ms (18.80%), Space: 15 MB (30.96%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,19 +1,20 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        ret=0
-        s = s.replace('))', ']')
-        ret+=s.count(')')
-        s = s.replace(')', ']')
-        bal = 0
+        opening, closing = 0, 0
+        i, n = 0, len(s)
         
-        for i, ch in enumerate(s):
-            if ch == '(':
-                bal += 1
-            elif ch == ']':
-                bal-=1
-            if bal == -1:
-                bal+=1
-                ret+=1
-        return ret + bal *2
-        
-        
+        while i < n:
+            if s[i] == '(':
+                opening += 1
+            elif s[i] == ')':
+                if i == n-1 or s[i+1] != ')':
+                    closing += 1
+                else:
+                    i+=1
+                
+                if opening:
+                    opening -= 1
+                else:
+                    closing += 1
+            i+=1
+        return opening * 2 + closing
