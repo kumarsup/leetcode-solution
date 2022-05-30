7365166a4d0fc9251f5acee1afb966569960697e
Sundeep Kumar <sundeepmalik.cse@gmail.com>
2022-05-30 23:03:02 +0800

    Time: 296 ms (17.53%), Space: 14.4 MB (99.50%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,11 +1,3 @@
-'''
-use two pointer approach - 
-if left != right - try left+1 or right-1
-    - any of this true then return true else return false
-TC- O(N)
-SC- O(1)
-
-'''
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         n = len(s)
@@ -14,13 +6,14 @@ class Solution:
         def isPalindrome(left, right):
             while left < right:
                 if s[left] != s[right]: return False
-                left, right = left+1, right-1
+                left+=1
+                right-=1
             return True
         
         while left < right:
-            if s[left] != s[right]: 
+            if s[left] != s[right]:
                 return isPalindrome(left+1, right) or isPalindrome(left, right-1)
-            left, right = left+1, right-1
+            left+=1
+            right-=1
         return True
-        
-        
+                
